c9367edeb844eaa6619cfae3f016df2d0d51d7c8
Noah-Huppert <contact@noahh.io>
2023-04-01 16:41:44 -0400

    init .NET MAUI project

---
+++ GlobalAgendaLauncher/GlobalAgendaLauncher/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GlobalAgendaLauncher;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ GlobalAgendaLauncher/GlobalAgendaLauncher/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GlobalAgendaLauncher;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
