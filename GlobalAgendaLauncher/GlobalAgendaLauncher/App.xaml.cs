012110a5a9b5ceaf61fe56e29c325064b2bfe9c4
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2023-04-06 22:01:55 -0400

    added title to window

--- GlobalAgendaLauncher/GlobalAgendaLauncher/App.xaml.cs
+++ GlobalAgendaLauncher/GlobalAgendaLauncher/App.xaml.cs
@@ -8,4 +8,15 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        if (window != null)
+        {
+            window.Title = "Global Agenda Launcher (Community)";
+        }
+
+        return window;
+    }
 }
